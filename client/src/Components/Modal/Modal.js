d7f3df254170da81ba88d933a73058c2857ccef7
Colin Parkhurst <parkhurstcolin@protonmail.com>
2024-12-16 18:28:32 -0500

    Input fields added to Modal

--- client/src/Components/Modal/Modal.js
+++ client/src/Components/Modal/Modal.js
@@ -4,11 +4,25 @@ import "./Modal.css";
 function Modal({ onClose, children }) {
 
 	return (
-		<div className='modal-overlay' onClick={onClose}>
-			<div className='modal-content' onClick={(e) => e.stopPropagation()}>
-				<button className='close-button' onClick={onClose}>
-					{children}
+		<div className="modal-overlay" onClick={onClose}>
+			<div className="modal-content" onClick={(e) => e.stopPropagation()}>
+				<button className="close-button" onClick={onClose}>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						strokeWidth={1.5}
+						stroke="currentColor"
+						className="size-6"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+						/>
+					</svg>
 				</button>
+				{children}
 			</div>
 		</div>
 	);
--- client/src/Components/ToDos/ToDos.js
+++ client/src/Components/ToDos/ToDos.js
@@ -3,7 +3,14 @@ import React from "react";
 const ToDos = () => {
 	return (
 		<div>
-			<p>ToDos</p>
+			<h1 className="text-3xl m-0 p-1">New To Do</h1>
+			<div className="m-0 p-1">
+				<>
+					<label>Please Enter:</label>
+					<input></input>
+				</>
+				<></>
+			</div>
 		</div>
 	);
 };

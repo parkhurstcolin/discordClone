17be4fc1b6322a99564556713b88197f1fb0b1bc
Colin Parkhurst <parkhurstcolin@protonmail.com>
2024-12-16 13:46:11 -0500

    Tailwind Installed

---
+++ client/src/Components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header";
+
+function App() {
+	return (
+		<div>
+			<Header />
+			<div>Body</div>
+		</div>
+	);
+}
+
+export default App;
---
+++ client/src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+	return (
+		<div>
+			<h1 className="text-3xl font-bold bg-red-500 text-white">To Do List</h1>
+		</div>
+	);
+}
+
+export default Header;

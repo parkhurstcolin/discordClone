2f43423b1fdf69d14453bff9263b0112db65775f
Colin Parkhurst <parkhurstcolin@protonmail.com>
2024-12-17 15:11:58 -0500

    actions setup

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,14 @@
 import axios from "axios";
 import { FETCH_TO_DO, SUBMIT_TO_DO } from "./types";
 
-export const submitToDo = () => async (dispatch) => {};
-export const fetchToDo = () => async (dispatch) => {};
+export const fetchToDo = () => async (dispatch) => {
+	const res = await axios.get("/api/todo");
+
+	dispatch({ type: FETCH_TO_DO, payload: res.data });
+};
+
+export const submitToDo = (values) => async (dispatch) => {
+	const res = await axios.post("/api/todo", values);
+
+	dispatch({ type: SUBMIT_TO_DO, payload: res.data });
+};

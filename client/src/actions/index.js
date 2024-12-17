8a99492d0ba0cf74207cccc5b3b290462ce301cc
Colin Parkhurst <parkhurstcolin@protonmail.com>
2024-12-17 15:07:25 -0500

    Fetch & Submit Requests started

---
+++ client/src/actions/index.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+import { FETCH_TO_DO, SUBMIT_TO_DO } from "./types";
+
+export const submitToDo = () => async (dispatch) => {};
+export const fetchToDo = () => async (dispatch) => {};
---
+++ client/src/actions/types.js
@@ -0,0 +1,2 @@
+export const SUBMIT_TO_DO = "submit-to-do";
+export const FETCH_TO_DO = "fetch-to-do";

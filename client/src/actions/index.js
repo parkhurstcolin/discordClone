174e16dae87e3499865ae082f82e63c5c88a8873
Colin Parkhurst <parkhurstcolin@protonmail.com>
2024-12-30 20:23:15 -0500

    o7 destroyed my bootloader

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,16 +1,15 @@
 import axios from "axios";
 import { FETCH_TO_DO, SUBMIT_TO_DO } from "./types";
-window.axios = axios;
 
 export const fetchToDo = () => async (dispatch) => {
-	const res = await axios.get("/api/todo");
+  const res = await axios.get("http://localhost:5000/api/todo");
 
-	dispatch({ type: FETCH_TO_DO, payload: res.data });
+  dispatch({ type: FETCH_TO_DO, payload: res.data });
 };
 
 export const submitToDo = (values) => async (dispatch) => {
-	const res = await axios.post("/api/todo", values);
-	console.log(res);
+  const res = await axios.post("http://localhost:5000/api/todo", values);
+  console.log(res);
 
-	dispatch({ type: SUBMIT_TO_DO, payload: res.data });
+  dispatch({ type: SUBMIT_TO_DO, payload: res.data });
 };

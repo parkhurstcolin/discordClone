81e04005a3fb016e8304c85c28ea7c94a0834a00
Colin Parkhurst <parkhurstcolin@protonmail.com>
2025-01-24 21:04:36 -0500

    Server component setup and strict mode setup

---
+++ client/src/components/Server.jsx
@@ -0,0 +1,13 @@
+import ServerChannel from "./ServerChannel";
+import Chat from "./Chat";
+
+const Server = () => {
+  return (
+    <>
+      <ServerChannel />
+      <Chat />
+    </>
+  );
+};
+
+export default Server;

ece0a645b1fb7412c89bca82f40fb916b55c9583
Colin Parkhurst <parkhurstcolin@protonmail.com>
2025-01-24 21:42:06 -0500

    rendering a passed array

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,19 @@
 import Servers from "./components/Servers";
 import Server from "./components/Server";
-
+import { useState } from "react";
+const DUMMY_SERVERS = [
+  { id: "5189785190865719026982786098267238907363867", name: "dummyServer1" },
+  { id: "6878502496578230496720395682626636367324698", name: "dummyServer2" },
+  { id: "1609876519386751925667373373737845673249872", name: "dummyServer3" },
+  { id: "0-34568740568469098234786909824865903465734", name: "dummyServer4" },
+];
 const App = () => {
+  const [serversList, setServersList] = useState([]);
   return (
     <div className="container">
-      <Servers />
+      <Servers serversList={serversList} />
       <Server />
+      <button onClick={() => setServersList(DUMMY_SERVERS)}>Render</button>
     </div>
   );
 };
--- client/src/components/Servers.jsx
+++ client/src/components/Servers.jsx
@@ -1,5 +1,25 @@
-const Servers = () => {
-  return <></>;
+import PropType from "prop-types";
+
+const Servers = ({ serversList }) => {
+  function RenderServersList() {
+    return serversList.map((server) => {
+      return (
+        <div key={server.id}>
+          <p>{server.name}</p>
+        </div>
+      );
+    });
+  }
+
+  return (
+    <div>
+      <RenderServersList />
+    </div>
+  );
+};
+
+Servers.propTypes = {
+  serversList: PropType.array,
 };
 
 export default Servers;

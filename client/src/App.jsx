e13153bd7da3faabecb70345474bf4b95cad2a68
Colin Parkhurst <parkhurstcolin@protonmail.com>
2025-01-25 11:30:29 -0500

    layout tweaking

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,14 +5,18 @@ const DUMMY_SERVERS = [
   { id: "5189785190865719026982786098267238907363867", name: "dummyServer1" },
   { id: "6878502496578230496720395682626636367324698", name: "dummyServer2" },
   { id: "1609876519386751925667373373737845673249872", name: "dummyServer3" },
-  { id: "0-34568740568469098234786909824865903465734", name: "dummyServer4" },
+  { id: "0234568740568469098234786909824865903465734", name: "dummyServer4" },
 ];
 const App = () => {
   const [serversList, setServersList] = useState([]);
   return (
-    <div className="container">
-      <Servers serversList={serversList} />
-      <Server />
+    <div className="container h-full grid columns-2 rows-auto">
+      <div className="h-full w-24">
+        <Servers serversList={serversList} />
+      </div>
+      <div className="h-full">
+        <Server />
+      </div>
       <button onClick={() => setServersList(DUMMY_SERVERS)}>Render</button>
     </div>
   );

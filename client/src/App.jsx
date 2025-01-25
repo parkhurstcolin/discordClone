69be2d6a180944f360e886a2fe2d4e7081ba60c0
Colin Parkhurst <parkhurstcolin@protonmail.com>
2025-01-24 20:52:23 -0500

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,5 @@
-import { useState } from "react";
-import Header from "./components/Header";
-import Modal from "./components/modal/Modal";
-import SubmitTodo from "./components/SubmitTodo";
-import ToDoList from "./components/ToDoList";
-
 const App = () => {
-  const [showModal, setShowModal] = useState(false);
-  const [toDos, setToDos] = useState([]);
-
-  function modalHandler() {
-    setShowModal(!showModal);
-  }
-
-  function addToDoHandler(todo) {
-    setToDos((todos) => [...todos, todo]);
-  }
-
-  return (
-    <div className="container w-3/5">
-      {showModal ? (
-        <Modal onClose={() => setShowModal(!showModal)}>
-          <SubmitTodo onAddToDo={addToDoHandler} />
-        </Modal>
-      ) : (
-        <></>
-      )}
-      <Header onNewPost={modalHandler} />
-      <ToDoList toDos={toDos} />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;

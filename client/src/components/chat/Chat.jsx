ba0628bf4565045a836f99d8062c46a57c2219ea
Colin Parkhurst <parkhurstcolin@protonmail.com>
2025-01-27 19:08:56 -0500

    Submitted an object to an array

--- client/src/components/chat/Chat.jsx
+++ client/src/components/chat/Chat.jsx
@@ -1,14 +1,18 @@
+import { useState } from "react";
 import Message from "./Message";
 
+let DUMMY_MESSAGES = [
+  { username: "Bob", message: "Testing Bob123", key: "56262623662" },
+  { username: "Bill", message: "Testing Bill123", key: "6262262622626" },
+  { username: "Bush", message: "Testing Bush123", key: "6264126672626262" },
+  { username: "Bubba", message: "Testing Bubba123", key: "62624242262626" },
+];
+
 const Chat = () => {
-  const DUMMY_MESSAGES = [
-    { username: "Bob", message: "Testing Bob123", key: "56262623662" },
-    { username: "Bill", message: "Testing Bill123", key: "6262262622626" },
-    { username: "Bush", message: "Testing Bush123", key: "6264126672626262" },
-    { username: "Bubba", message: "Testing Bubba123", key: "62624242262626" },
-  ];
+  const [sendMessage, setSendMessage] = useState("");
+  const [messages, setMessages] = useState([]);
   function renderChatMessages() {
-    return DUMMY_MESSAGES.map((message) => {
+    return DUMMY_MESSAGES.concat(messages).map((message) => {
       return (
         <Message
           username={message.username}
@@ -18,7 +22,32 @@ const Chat = () => {
       );
     });
   }
-  return <>{renderChatMessages()}</>;
+
+  function sendMessageHandler(data) {
+    const messageObject = [
+      {
+        username: "Boe",
+        message: data,
+        key: String(Date.now()),
+      },
+    ];
+    setMessages(...messages, messageObject);
+    setSendMessage("");
+  }
+
+  return (
+    <div>
+      <div>{renderChatMessages()}</div>
+      <div>
+        <input
+          type="text"
+          value={sendMessage}
+          onChange={(event) => setSendMessage(event.target.value)}
+        ></input>
+        <button onClick={() => sendMessageHandler(sendMessage)}>Send</button>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
